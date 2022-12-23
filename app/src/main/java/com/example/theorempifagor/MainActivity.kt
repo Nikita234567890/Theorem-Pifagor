be3bec5edc2ae21e8809f3c3b2cb9de03b2a93de
Nikita234567890 <117517437+Nikita234567890@users.noreply.github.com>
2022-12-23 18:11:59 +0300

    Done program pifagor

--- app/src/main/java/com/example/theorempifagor/MainActivity.kt
+++ app/src/main/java/com/example/theorempifagor/MainActivity.kt
@@ -8,34 +8,35 @@ import kotlin.math.pow
 import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding : ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 
-    fun onClick(view : View){
-        var res = getString(R.string.Result) + getResult()
-        if (!isEmpty()) binding.tvResult.text = res
-        
-    }
-
-   private fun isEmpty(): Boolean{
-        binding.apply {
-            if (edA.text.isNullOrEmpty()) edA.error = getString(R.string.Error)
-            if (edB.text.isNullOrEmpty()) edB.error = getString(R.string.Error)
-            return edA.text.isNullOrEmpty() && edB.text.isNullOrEmpty()
-
+    fun onClickResult(view: View){
+        if(!isFieldEmpty()){
+            val result = getString(R.string.result_info) + getResult()
+            binding.tvResult.text = result
         }
     }
-    private fun getResult ():String{
-        var a: Double
-        var b: Double
+
+    private fun isFieldEmpty(): Boolean{
+        binding.apply {
+            if(edA.text.isNullOrEmpty()) edA.error = "Поле должно быть заполнено"
+            if(edB.text.isNullOrEmpty()) edB.error = "Поле должно быть заполнено"
+            return  edA.text.isNullOrEmpty() || edB.text.isNullOrEmpty()
+        }
+    }
+
+    private fun getResult(): String{
+        val a: Double
+        val b: Double
         binding.apply {
             a = edA.text.toString().toDouble()
             b = edB.text.toString().toDouble()
-            return sqrt(a.pow(2) + b.pow(2)).toString()
         }
+        return sqrt((a.pow(2) + b.pow(2))).toString()
     }
 }
